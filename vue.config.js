3d52f1366b207c520a549e6a1306c7c31ada7e73
zruu <3400018227@qq.com>
2023-05-13 17:29:20 +0800

    change something

--- vue.config.js
+++ vue.config.js
@@ -37,6 +37,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
+  // change something
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
